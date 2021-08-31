853580aea27b77b2d19c04073ebf6bec40411cd3
Alex Renoki <rennokki@gmail.com>
2021-08-31 09:06:21 +0300

    Testing

--- tests/HelmTest.php
+++ tests/HelmTest.php
@@ -29,4 +29,41 @@ class HelmTest extends TestCase
 
         $this->assertTrue($process->isSuccessful());
     }
+
+    public function test_helm_repo_install()
+    {
+        $process = Helm::install(
+            'test-release',
+            'stable/chart-name',
+            ['--debug' => true]
+        );
+
+        $this->assertStringContainsString(
+            'install test-release "stable/chart-name" --debug',
+            $process->getCommandLine()
+        );
+    }
+
+    public function test_helm_repo_upgrade()
+    {
+        $process = Helm::upgrade(
+            'test-release',
+            'stable/chart-name',
+            ['--debug' => true]
+        );
+
+        $this->assertStringContainsString(
+            'upgrade test-release "stable/chart-name" --debug',
+            $process->getCommandLine()
+        );
+    }
+
+    public function test_helm_repo_macro()
+    {
+        Helm::macro('test', function ($var) {
+            return $var;
+        });
+
+        $this->assertEquals('123', Helm::test('123'));
+    }
 }
